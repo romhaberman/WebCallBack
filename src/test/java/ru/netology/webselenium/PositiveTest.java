95c1b04503a83cf47e05f9c3c2b8cd16f1dc87f8
Roman Khabarov <habermanrom@gmail.com>
2024-08-12 19:40:50 +1000

    error fixes

--- src/test/java/ru/netology/webselenium/PositiveTest.java
+++ src/test/java/ru/netology/webselenium/PositiveTest.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
